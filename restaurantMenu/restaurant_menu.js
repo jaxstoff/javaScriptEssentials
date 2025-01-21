2202325cff27cf0527b94e7a787b5cc1c6b7791b
jaxstoff <acstoffel@gmail.com>
2025-01-21 16:14:24 -0500

    Update restaurant_menu.js with prices

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const mainCourseMenu = ['Steak- $30.99', 'Pasta- $15.25', 'Burger- $12.99', 'Salmon- $25.50'];
+const dessertMenu = ['Cake- $10', 'Ice Cream- $5.99', 'Pudding', 'Fruit Salad- $8.00'];
 
 // breakfast menu
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
